c4fbfe45682644ad0194eef9eec8524f1ea5724a
Richard Cargill <rcargill@dunelm.com>
2020-06-07 17:06:52 +0100

    tidying up presentational panel

--- src/components/Panels/PresentationPanel.js
+++ src/components/Panels/PresentationPanel.js
@@ -1,26 +1,28 @@
 import React from 'react'
 
 const PresentationPanel = ({ iframe, fullWidth, text }) => {
-  const presentationClass = `container presentation panel ${
+  const presentationClass = `container panel ${
     fullWidth !== true ? 'limit-width' : ''
   }`
   const url = `${iframe.internal.content}`
   const width = 960
   const height = 569
-  const linkText = text || 'View presentation'
+  const linkText = text || 'Continue to case study'
+  const linkUrl = url.replace('&rm=minimal', '')
   return (
     <section className={presentationClass}>
-      <iframe
-        className="mobile-is-hidden"
-        src={url}
-        width={width}
-        height={height}
-        allowFullScreen="true"
-        mozallowfullscreen="true"
-        webkitallowfullscreen="true"
-        frameBorder="0"
-      />
-      <a href={url} target="_blank">
+      <div className="mobile-is-hidden presentation">
+        <iframe
+          src={url}
+          width={width}
+          height={height}
+          allowFullScreen="true"
+          mozallowfullscreen="true"
+          webkitallowfullscreen="true"
+          frameBorder="0"
+        />
+      </div>
+      <a className="large-is-hidden" href={linkUrl} target="_blank">
         {linkText}
       </a>
     </section>
